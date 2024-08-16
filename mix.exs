149730dd8b884e00475d8a4b7607e7e4212ef3ec
Brian Underwood <public@brian-underwood.codes>
2024-08-16 15:23:15 +0200

    chore: mix test.watch

--- mix.exs
+++ mix.exs
@@ -9,7 +9,10 @@ defmodule SetGame.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        "test.watch": :test
+      ]
     ]
   end
 
@@ -58,7 +61,8 @@ defmodule SetGame.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
